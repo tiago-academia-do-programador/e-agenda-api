9634db086d2b9d90125c37b493c7bc3325a70446
Alexandre Rech <alexandre.rech.lages@gmail.com>
2022-10-16 22:07:14 -0300

    Removido a migração automática do banco de dados

--- eAgenda.Webapi/Program.cs
+++ eAgenda.Webapi/Program.cs
@@ -12,7 +12,7 @@ namespace eAgenda.Webapi
         public static void Main(string[] args)
         {
             ConfiguracaoLogseAgenda.ConfigurarEscritaLogs();
-            MigradorBancoDadoseAgenda.AtualizarBancoDados();
+            //MigradorBancoDadoseAgenda.AtualizarBancoDados();
 
             Log.Logger.Information("Iniciando o servidor da aplicação e-Agenda...");
 
