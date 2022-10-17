0bf52af87af35fa43ba8e0ebf930fe36a865fdf2
Alexandre Rech <alexandre.rech.lages@gmail.com>
2022-10-16 22:59:33 -0300

    Adicionado DataCriacao na visualização da lista de tarefas

--- eAgenda.Webapi/ViewModels/ModuloTarefa/ListarTarefaViewModel.cs
+++ eAgenda.Webapi/ViewModels/ModuloTarefa/ListarTarefaViewModel.cs
@@ -8,6 +8,8 @@ namespace eAgenda.Webapi.ViewModels.ModuloTarefa
 
         public string Titulo { get; set; }
 
+        public DateTime DataCriacao { get; set; }
+
         public string Prioridade { get; set; }
 
         public string Situacao { get; set; }
